cc6eba93d266413771008043125f59c00da43f12
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2022-07-05 05:39:24 -0400

    Create cprint.c

---
+++ cprint.c
@@ -0,0 +1,13 @@
+int main()
+{
+    
+    int number1 = 5;
+    
+    printf("Number = %d \n", number1);
+    
+    double number2 = 75.8;
+
+    printf("number2 = %lf", number2);
+    
+    return 0;
+}
